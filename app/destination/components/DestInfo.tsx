e51e7b6ca74c5b792e9074b2aff71cb186a02ad5
Dev-Dylann <tubitunmise@gmail.com>
2024-09-13 20:00:46 +0100

    finished md and lg styling for destination page

--- app/destination/components/DestInfo.tsx
+++ app/destination/components/DestInfo.tsx
@@ -33,38 +33,38 @@ export default function DestinationInfo({ children, destination }: Props) {
     }, [destination])
 
     return (
-        <>
+        <div className='flex flex-col gap-8 md:gap-12 items-center lg:grid lg:grid-cols-2 lg:grid-rows-[repeat(5,_auto)] lg:gap-y-8 lg:gap-x-16 lg:my-auto xl:gap-x-24'>
             <Image
                 src={data?.images.png as string}
                 alt={`Destination: ${data?.name}`}
                 width={481}
                 height={480}
-                className="w-1/2 my-7 transition-all duration-500"
+                className="w-1/2 my-7 transition-all duration-500 lg:row-span-full lg:w-full"
                 style={fade ? { opacity: 0 } : { opacity: 1 }}
             />
 
             {children}
 
-            <article className="flex flex-col gap-5 items-center text-center transition-all duration-500" style={fade ? { opacity: 0 } : { opacity: 1 }}>
-                <h2 className={`${bellefair.className} text-[65px]`}>{data?.name.toUpperCase()}</h2>
+            <article className="flex flex-col gap-5 items-center text-center transition-all duration-500 md:gap-1 lg:text-left lg:items-start" style={fade ? { opacity: 0 } : { opacity: 1 }}>
+                <h2 className={`${bellefair.className} text-[65px] md:text-[80px]`}>{data?.name.toUpperCase()}</h2>
 
-                <p className='leading-8'>{data?.description}</p>
+                <p className='leading-8 text-blue-300 md:text-lg md:leading-10 md:max-w-[75vw]'>{data?.description}</p>
             </article>
 
-            <div className='w-full border mt-4 opacity-65'></div>
+            <div className='w-full border-white border mt-4 opacity-25 md:max-w-[75vw] md:mt-0'></div>
 
-            <article className='flex flex-col gap-5 text-center items-center transition-all duration-500' style={fade ? { opacity: 0 } : { opacity: 1 }}>
-                <div className='flex flex-col gap-2'>
+            <article className='flex flex-col gap-5 text-center items-center transition-all duration-500 md:flex-row md:w-full md:justify-between md:max-w-[75vw] lg:text-left lg:items-start' style={fade ? { opacity: 0 } : { opacity: 1 }}>
+                <div className='flex flex-col gap-2 w-full'>
                     <p className={`${barlowCondensed.className} tracking-widest opacity-75`}>AVG. DISTANCE</p>
                     <p className={`${bellefair.className} text-3xl`}>{data?.distance.toUpperCase()}</p>
                 </div>
 
-                <div className='flex flex-col gap-2'>
+                <div className='flex flex-col gap-2 w-full'>
 
                     <p className={`${barlowCondensed.className} tracking-widest opacity-75`}>EST. TRAVEL TIME</p>
                     <p className={`${bellefair.className} text-3xl`}>{data?.travel.toUpperCase()}</p>
                 </div>
             </article>
-        </>
+        </div>
     )
 }
--- app/destination/page.tsx
+++ app/destination/page.tsx
@@ -27,16 +27,16 @@ export default function Destination({ searchParams }: Props) {
             {/* Background Image */}
             <div className="absolute top-0 left-0 w-full h-full -z-[1] bg-cover bg-[50%] bg-fixed" style={{ backgroundImage: "url('/assets/destination-bg-desktop.png')" }}></div>
 
-            <section className='grow flex flex-col gap-8 pt-28 pb-6 px-6 items-center'>
-                <h1 className={`${barlowCondensed.className} flex gap-5 tracking-widest text-lg`}>
+            <section className='grow flex flex-col gap-8 pt-28 pb-6 px-6 items-center md:pt-32 md:px-8 md:gap-12 md:pb-12 lg:px-10 lg:max-w-6xl lg:mx-auto lg:pt-40'>
+                <h1 className={`${barlowCondensed.className} flex gap-5 tracking-widest text-lg md:text-left md:w-full md:text-xl lg:text-[28px]`}>
                     <span className='font-bold opacity-25'>01</span>
                     PICK YOUR DESTINATION
                 </h1>
 
                 <DestinationInfo destination={currentDestination!}>
-                    <div className={`${barlowCondensed.className} flex justify-center gap-10 tracking-widest`}>
+                    <div className={`${barlowCondensed.className} flex justify-center gap-10 tracking-widest md:text-lg lg:justify-start`}>
                         {destinations.map((destination, index) => (
-                            <Link key={`destination${index}`} href={`?dest=${destination.name.toLowerCase()}`} className={destination.name.toLowerCase() === dest || dest === undefined && index === 0 ? 'border-b-2 pb-2' : 'pb-2 opacity-50'}>
+                            <Link key={`destination${index}`} href={`?dest=${destination.name.toLowerCase()}`} className={destination.name.toLowerCase() === dest || dest === undefined && index === 0 ? 'border-b-2 pb-2 transition-all' : 'pb-2 text-blue-300 hover:border-b-2 hover:border-blue-300 transition-all'}>
                                 {destination.name.toUpperCase()}
                             </Link>
                         ))}
