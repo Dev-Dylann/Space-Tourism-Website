90cf27629ab83c090f4ed6c41a0b3740ca97e153
Dev-Dylann <tubitunmise@gmail.com>
2024-09-13 18:08:07 +0100

    finished lg styling for home page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -14,8 +14,8 @@ export default function Header() {
     const links = ['home', 'destination', 'crew', 'technology']
 
     return (
-        <header className={`${barlowCondensed.className} p-6 flex justify-between items-center fixed top-0 left-0 w-full z-20 md:justify-start md:p-0`}>
-            <img src="/assets/logo.svg" alt="Logo" className='md:mx-10 md:my-6' />
+        <header className={`${barlowCondensed.className} p-6 flex justify-between items-center fixed top-0 left-0 w-full z-20 md:justify-start md:p-0 lg:py-8 lg:justify-between xl:py-10`}>
+            <img src="/assets/logo.svg" alt="Logo" className='md:mx-8 md:my-6 lg:mx-12 xl:mx-16' />
 
             <button onClick={() => setNavState(true)} className="px-2 rounded-md md:hidden">
                 <img src="/assets/hamburger.svg" alt="Menu Icon" />
@@ -23,8 +23,8 @@ export default function Header() {
 
             <MobileNav navState={navState} setNavState={setNavState} path={path} links={links} />
 
-            <section className='hidden md:block bg-white bg-opacity-5 self-stretch grow backdrop-blur-2xl'>
-                <nav className='h-full px-8 text-lg flex items-center justify-end gap-10 tracking-widest'>
+            <section className='hidden md:block bg-white bg-opacity-5 self-stretch grow backdrop-blur-2xl lg:grow-0'>
+                <nav className='h-full px-8 text-lg flex items-center justify-end gap-10 tracking-widest before:hidden lg:pr-12 lg:pl-28 xl:px-16 xl:w-[60vw] lg:relative lg:before:block before:absolute before:w-[25vw] before:border-[0.5px] before:border-white before:-left-[35%] xl:before:w-[50%] xl:before:-left-[45%]'>
                     {links.map((link, index) => (
                         <Link key={`link${index}`} href={link !== 'home' ? link : '/'} className={path.includes(link) || path === '/' && index === 0 ? 'border-b-4 transition-all h-full flex items-center' : 'hover:border-b-4 hover:border-blue-300 transition-all h-full flex items-center'}>
                             <span className="font-bold">0{index}</span>
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,42 @@
 import Image from "next/image"
 import { barlowCondensed, bellefair } from "./ui/fonts"
+import Link from "next/link"
 
 export default function Home() {
   return (
     <main className="relative flex grow overflow-y-hidden">
 
-      {/* Background Image */}
+      {/* Background Image - mobile and tablets */}
       <Image
         src="/assets/home-bg-mobile.png"
         alt="Space Background"
         fill
-        className="-z-[1]"
+        className="-z-[1] lg:hidden"
       />
 
-      <section className="grow flex flex-col pt-32 pb-12 px-6 items-center text-center md:pt-40 md:px-8 md:justify-center">
-        <p className={`${barlowCondensed.className} text-lg tracking-widest text-blue-300 md:text-xl`}>SO, YOU WANT TO TRAVEL TO</p>
+      {/* Background Image - desktop */}
+      <Image
+        src="/assets/home-bg-desktop.png"
+        alt="Space Background"
+        fill
+        className="-z-[1] hidden lg:block"
+      />
 
-        <p className={`${bellefair.className} text-[100px] md:text-[124px]`}>SPACE</p>
+      <section className="grow flex flex-col pt-32 pb-12 px-6 items-center text-center md:pt-40 md:px-8 md:justify-center lg:flex-row lg:justify-between lg:items-end lg:pb-20 lg:px-10 lg:text-left lg:gap-10 max-w-6xl mx-auto xl:pb-32">
+        <article className='flex flex-col'>
+          <p className={`${barlowCondensed.className} text-lg tracking-widest text-blue-300 md:text-xl`}>SO, YOU WANT TO TRAVEL TO</p>
 
-        <p className='leading-8 text-lg text-blue-300 md:px-10 md:text-base md:leading-10'>
-          Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hiver kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!
-        </p>
+          <p className={`${bellefair.className} text-[100px] md:text-[124px]`}>SPACE</p>
 
-        <div className='grow w-full grid place-content-center md:grow-0 md:py-12'>
-          <button className={`${bellefair.className} text-xl text-blue-900 rounded-full w-36 h-36 bg-white grid place-content-center md:w-60 md:h-60 md:text-3xl`}>
+          <p className='leading-8 text-lg text-blue-300 md:px-10 md:text-base md:leading-10 lg:px-0'>
+            Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hiver kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!
+          </p>
+        </article>
+
+        <div className='grow w-full grid place-content-center md:grow-0 md:py-12 lg:place-content-end'>
+          <Link href="/destination" className={`${bellefair.className} text-xl text-blue-900 rounded-full w-36 h-36 bg-white grid place-content-center md:w-60 md:h-60 md:text-3xl hover:scale-90 transition-all duration-700 relative before:absolute before:w-full before:h-full before:top-0 before:left-0 before:rounded-full before:bg-white before:bg-opacity-50 hover:before:scale-150 before:-z-[1] before:transition-all before:duration-700`}>
             EXPLORE
-          </button>
+          </Link>
         </div>
       </section>
 
